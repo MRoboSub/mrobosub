b92ded4e9894c23cd981564d7cda4ccc3de88ea9
Nolan Kuza <nkuza8@gmail.com>
2022-10-15 17:52:04 -0400

    [fcu] Use NoChange in resize

--- mrobosub_fcu/src/ThrusterManager.cpp
+++ mrobosub_fcu/src/ThrusterManager.cpp
@@ -2,7 +2,7 @@
 
 void ThrusterManager::calculate_thruster_matrices() {
     // Number of columns is number of thrusters
-    thrusts_to_screws.resize(thrusts_to_screws.rows(), thrusters.size());
+    thrusts_to_screws.resize(Eigen::NoChange, thrusters.size());
     for(size_t i = 0; i < thrusters.size(); ++i) {
         thrusts_to_screws.col(i) = thrusters[i].get_contribution().as_vector6();
     }
