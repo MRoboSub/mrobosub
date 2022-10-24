cef40af6fda2f77a91bfca10fcd53caa73716cc3
evangebo <evangebo@umich.edu>
2022-10-23 22:07:07 -0400

    updated state machine to remove handlers and added example

--- mrobosub_planning/src/example.py
+++ mrobosub_planning/src/example.py
@@ -1,47 +1,67 @@
 from state_machine import *
 
 
-StartOutcome = make_outcome('Start')
-SubmergeOutcome = make_outcome('Submerge', at_target=False, timeout=False)
-TurnOutcome = make_outcome('Turn', at_target=False, timeout=False)
-StopOutcome = make_outcome('Stop', exit_with_timout=False)
+class Start(State):
+    Complete = Outcome.make('Complete')
 
-SubmergeReachedDepth = make_outcome('SubmergedReachedDepth')
-SubmergeReachedDepth = make_outcome('SubmergedFailedReachedDepth')
-SubmergeReachedDepth = make_outcome('SubmergedReachedDepth')
+    def initialize(self, prev_outcome: Outcome, gbl_ctx: Context) -> None:
+        print('Startup...')
 
-
-class StartHandler(StateHandler):
-    def iteration(self, gbl_ctx: Context):
-        pass
+    def handle(self, gbl_ctx: Context) -> Outcome:
+        return self.Complete()
 
 
 class Submerge(State):
-    outcomes = [
-        Outcome('ReachedDepth'),
-    ]
+    Timeout = Outcome.make('Timeout')
+    ReachedTarget = Outcome.make('ReachedTarget')
+    FailedToReachTarget = Outcome.make('FailedToReachTarget')
 
-    def iteration(self):
+    def initialize(self, prev_outcome: Outcome, gbl_ctx: Context) -> None:
+        self.depth = 0
+
+    def handle(self, gbl_ctx: Context) -> Outcome:
+        if self.depth == 10:
+            return self.ReachedTarget()
+        else:
+            self.depth += 1
+            return self.FailedToReachTarget()
+
+
+class Turn(State):
+    Timeout = Outcome.make('Timeout')
+    ReachedTarget = Outcome.make('ReachedTarget')
+    FailedToReachTarget = Outcome.make('FailedToReachTarget')
+
+    def initialize(self, prev_outcome: Outcome, gbl_ctx: Context) -> None:
         pass
 
-
-class SubmergeHandler(StateHandler):
-    def iteration(self, gbl_ctx: Context):
-        pass
+    def handle(self, gbl_ctx: Context) -> Outcome:
+        return self.Timeout()
 
 
-class TurnHandler(StateHandler):
-    def iteration(self, gbl_ctx: Context):
-        pass
+class Stop(State):
+    Success = Outcome.make('Success', outcome=None)
+    Failure = Outcome.make('Failure', outcome=None)
 
+    def initialize(self, prev_outcome: Outcome, gbl_ctx: Context) -> None:
+        print('Teardown...')
+        self.outcome = prev_outcome
+        self.error = type(prev_outcome) in (Submerge.Timeout, Turn.Timeout)
 
-class StopHandler(StateHandler):
-    def iteration(self, gbl_ctx: Context):
-        pass
+    def handle(self, gbl_ctx: Context) -> Outcome:
+        return self.Failure(self.outcome) if self.error else self.Success(self.outcome)
 
 
 if __name__ == '__main__':
-    Start = State('Start', StartHandler)
-    Submerge = State('Submerge', SubmergeHandler)
-    Turn = State('Turn', TurnHandler)
-    Stop = State('Stop', StopHandler)
+    transitions = {
+        Start.Complete: Submerge,
+        Submerge.ReachedTarget: Turn,
+        Submerge.FailedToReachTarget: Submerge,
+        Submerge.Timeout: Stop,
+        Turn.ReachedTarget: Stop,
+        Turn.FailedToReachTarget: Turn,
+        Turn.Timeout: Stop,
+    }
+    state_machine = StateMachine(transitions, Start, Stop)
+    outcome = state_machine.run(None)
+    print(outcome)
--- mrobosub_planning/src/state_machine.py
+++ mrobosub_planning/src/state_machine.py
@@ -2,88 +2,41 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from dataclasses import make_dataclass, field
 from typing import Mapping, Type
-# import rospy
+import rospy
+
+
+__all__ = ['Outcome', 'State', 'Context', 'StateMachine']
 
 
 class Outcome(ABC):
-    def __hash__(self):
-        return hash(type(self))
-
-    def __str__(self):
-        return self.__class__.__name__
-
     @classmethod
-    def __new__(cls, *args, **kwargs):
-        return make_dataclass(
-            args[0],
-            [(key, type(value), field(default=value)) for key, value in kwargs.items()],
-            bases=(cls,),
-        )
-    
-
-def make_outcome(name, **default_fields):
-    return make_dataclass(
-        name,
-        [(key, type(value), field(default=value)) for key, value in default_fields.items()],
-        bases=(Outcome, ),
-    )
+    def make(cls, name: str, **kwargs):
+        fields = [(key, type(val), field(default=val)) for key, val in kwargs.items()]
+        return make_dataclass(name, fields, bases=(cls, ))
 
 
-class SubState:
-    outcome1 = None
-    outcome2 = None
-
-
-class State2(ABC):
-    def __int__(self, prev_outcome: Outcome, gbl_ctx: Context):
+class State(ABC):
+    def __init__(self, prev_outcome: Outcome, gbl_ctx: Context):
         self.initialize(prev_outcome, gbl_ctx)
 
     @abstractmethod
-    def initialize(self, prev_outcome: Outcome, gbl_ctx: Context):
+    def initialize(self, prev_outcome: Outcome, gbl_ctx: Context) -> None:
         pass
 
     @abstractmethod
-    def handle(self, gbl_ctx: Context):
+    def handle(self, gbl_ctx: Context) -> Outcome:
         pass
-
-
-class State:
-    last_handled_state = None
-
-    def __init__(self, name: str, HandlerType: Type[StateHandler]):
-        self.HandlerType = HandlerType
-        self.name = name
-
-    def handle(self, outcome: Outcome, gbl_ctx: Context):
-        if self.last_handled_state is not self:
-            self.handler = self.HandlerType(outcome, gbl_ctx)
-
-        State.last_handled_state = self
-        return self.handler.iteration(gbl_ctx)
-
-    def __eq__(self, other):
-        return self.name == other.name
 
 
 class Context:
     def __init__(self, param_name):
-        params = []#rospy.getparam(param_name)
+        params = rospy.getparam(param_name)
         for key in params:
             setattr(self, key, params[key])
 
 
-class StateHandler(ABC):
-    def __init__(self, prev_outcome: Outcome, gbl_ctx: Context):
-        self.prev_outcome = prev_outcome
-        self.gbl_ctx = gbl_ctx
-
-    @abstractmethod
-    def iteration(self, gbl_ctx: Context):
-        pass
-
-
 class StateMachine:
-    def __init__(self, transitions: Mapping[Type[Outcome], Type[State2]], StartState: Type[State2], StopState: Type[State2]):
+    def __init__(self, transitions: Mapping[Type[Outcome], Type[State]], StartState: Type[State], StopState: Type[State]):
         self.StartState = StartState
         self.transitions = transitions
         self.StopState = StopState
@@ -96,21 +49,3 @@ class StateMachine:
             if type(current_state) != NextState:
                 current_state = NextState(outcome, gbl_ctx)
         return current_state.handle(gbl_ctx)  # handle stop state
-
-
-if __name__ == '__main__':
-    Timeout = make_outcome('Timeout')
-    NotThere = make_outcome('NotThere', error=15)
-    ReachDepth = make_outcome('ReachDepth', depth=78)
-
-    to = Timeout()
-    nt = NotThere(error=12)
-    rd = ReachDepth()
-
-    print(to)
-    print(nt.__dict__)
-    print(nt)
-    print(rd.__dict__)
-
-
-
