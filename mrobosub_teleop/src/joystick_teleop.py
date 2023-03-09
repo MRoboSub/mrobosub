47d559ec08faedbc3009f81633ff9aa77cdb4100
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2023-03-08 20:25:21 -0500

    Working on framework

--- mrobosub_teleop/src/joystick_teleop.py
+++ mrobosub_teleop/src/joystick_teleop.py
@@ -2,6 +2,9 @@
 
 import rospy
 from sensor_msgs.msg import Joy
+from std_msgs.msg import Header, Float64
+from genpy import Message
+from math import atan2
 
 from mrobosub_lib.lib import Node, Param
 
@@ -24,29 +27,72 @@ class JoystickTeleop(Node):
     - /joy
     """
 
+    # JSON-like objects
     surge: ...
     sway: ...
     heave: ...
+    roll: ...
+    pitch: ...
     yaw: ...
 
     def __init__(self):
         super().__init__("joystick_teleop")
         self.joystick_subscriber = rospy.Subscriber("/joy", Joy, self.callback)
-        self.depth_to_hold = None
-        self.yaw_to_hold = None
-        self.roll_to_hold = None
-        self.pitch_to_hold = None
+
+        self.periodic_funcs = {}
+        self.periodic_funcs['surge'] = lambda: None
+        self.periodic_funcs['sway'] = lambda: None
+        self.periodic_funcs['heave'] = lambda: None
+        self.periodic_funcs['roll'] = lambda: None
+        self.periodic_funcs['pitch'] = lambda: None
+        self.periodic_funcs['yaw'] = lambda: None
+
+        self.axes = []
+        self.buttons = []
+
+        self.publisher_map = {}
 
     def callback(self, msg: Joy):
-        print(msg.axes, msg.buttons)
-        print(f'{self.surge}')
-        print(f'{self.sway}')
-        print(f'{self.heave}')
-        print(f'{self.yaw}')
+        self.axes = msg.axes
+        self.buttons = msg.buttons
+        print(msg)
+
+    def get_button(self, id: int) -> bool:
+        if len(self.buttons) <= id:
+            return False
+        return self.buttons[id]
+
+    def get_axis(self, id: int) -> float:
+        if len(self.axes) <= id:
+            return 0
+        return self.axes[id]
+
+    def get_publisher(self, topic: str, msg_type: Message) -> rospy.Publisher:
+        if topic not in self.publisher_map:
+            self.publisher_map[topic] = rospy.Publisher(topic, msg_type, queue_size=1)
+        return self.publisher_map[topic]
+
+    def periodic(self):
+        for func in self.periodic_funcs.values():
+            func()
+
+    def enable_joystick_yaw(self):
+        yaw_publisher = self.get_publisher("/target_pose/yaw", Float64)
+        def periodic():
+            angle = atan2() # TODO
+            yaw_publisher.publish(angle)
+        self.periodic_funcs['yaw'] = periodic
+
+    def print_controls(self):
+        print(self.surge)
+        print(self.sway)
+        print(self.heave)
+        print(self.yaw)
 
     def run(self):
-        rospy.spin()
-
+        while not rospy.is_shutdown():
+            self.periodic()
+            rospy.sleep(0.05)
 
 if __name__ == "__main__":
     JoystickTeleop().run()
