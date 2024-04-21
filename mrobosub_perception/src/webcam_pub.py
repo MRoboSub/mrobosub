739f25d99d249f59d32546479f684e81ceb0a780
Jetson <michiganroboticsubmarine@gmail.com>
2024-04-21 16:15:26 -0400

    Fixes to switching cameras from testing on the sub

--- mrobosub_perception/src/webcam_pub.py
+++ mrobosub_perception/src/webcam_pub.py
@@ -5,36 +5,47 @@ from sensor_msgs.msg import Image
 import cv2
 from cv_bridge import CvBridge
 
-from std_srvs.srv import SetBool, SetBoolRequest
+from std_srvs.srv import SetBool, SetBoolRequest, SetBoolResponse
 
 class WebcamPub():
     def __init__(self) -> None:
         self.on = False
 
     def handle_on_service(self, req: SetBoolRequest):
+        if req.data and not self.on:
+            self.open_capture()
+        elif not req.data and self.on:
+            self.close_capture()
         self.on = req.data
+        return SetBoolResponse(success=True)
+
+    def open_capture(self):
+        self.cap = cv2.VideoCapture(self.device_path)
+        # https://stackoverflow.com/a/66279297
+        #cap.set(cv2.CAP_PROP_FPS,10)
+        #cap.set(cv2.CAP_PROP_BUFFERSIZE,1)
+        self.cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.75)
+        self.cap.set(cv2.CAP_PROP_EXPOSURE, 100)
+
+    def close_capture(self):
+        self.cap.release()
 
     def run(self):
         rospy.init_node('bot_pub_py', anonymous=True)
 
-        pub = rospy.Publisher('bot_cam', Image, queue_size=10)
+        pub = rospy.Publisher('bot_cam', Image, queue_size=1)
         rospy.Service('/bot_cam/on', SetBool, self.handle_on_service)
 
         # Create a VideoCapture object
-        cap = cv2.VideoCapture("/dev/video10")
-        # https://stackoverflow.com/a/66279297
-        cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.75)
-        cap.set(cv2.CAP_PROP_EXPOSURE, 100)
-        #cap.set(cv2.CAP_PROP_FPS,10)
-        #cap.set(cv2.CAP_PROP_BUFFERSIZE,1)
+        self.device_path = "/dev/video10"
 
-        rate = rospy.Rate(10)
+        rate = rospy.Rate(30)
 
         br = CvBridge()
 
         while not rospy.is_shutdown():
             if self.on:
-                ret, frame = cap.read()
+                ret, frame = self.cap.read()
 
                 if ret == True:
                     pub.publish(br.cv2_to_imgmsg(frame, encoding='bgr8'))
