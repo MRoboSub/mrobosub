8efbe37ea39d0efe4e66298b0e8212b9123c368e
Bhavye Mathur <bhavyemathur@gmail.com>
2024-09-22 15:03:42 -0400

    Revert "Removed test folder"
    This reverts commit 246813a77dde61d4756abe1813bf6b5be3847a8f.

---
+++ mrobosub_planning/src/test/bin_states_test.py
@@ -0,0 +1,75 @@
+from enum import Enum, auto
+import gc
+from utils import binCamPosServiceMock, TargetReader, PoseMock
+from typing import Dict, List
+from std_msgs.msg import Float64, String
+from mrobosub_msgs.srv import ObjectPosition, ObjectPositionResponse, BinCamPosResponse  # type: ignore
+from time import sleep
+from functools import partial
+import rospy
+
+def main():
+    rospy.init_node('test_sim')
+    started = False
+    sub = None
+    def set_started(msg: String):
+        nonlocal started, sub
+        if started:
+            return
+        print(msg.data)
+        started = True
+        if sub is not None:
+            sub.unregister()
+        del sub
+    sub = rospy.Subscriber('/captain/current_state', String, set_started)
+
+    while not started:
+        sleep(0.1)
+    print('Started!')
+    sleep(0.5)
+
+    bin_cam_mock = binCamPosServiceMock()
+    reader = TargetReader()
+    pose = PoseMock()
+
+    pose.heave = 0.
+
+    while pose.heave < reader.target_pose_heave:
+        pose.heave += 0.005
+        #print(f"{pose.heave=}")
+        sleep(0.05)
+        pose.publish_update()
+
+    for i in range(20*3):
+        sleep(0.05)
+
+    bin_cam_instance = BinCamPosResponse()
+    bin_cam_instance.found = True
+    bin_cam_instance.x = -100
+    bin_cam_instance.y = 173
+
+    bin_cam_mock.set_position(bin_cam_instance)
+
+    pose.yaw = 50
+    pose.publish_update()
+
+    for i in range(20*3):
+        
+        sleep(0.05)
+        
+        #print(f"{reader=}")
+        print(f"{reader.target_pose_yaw=}")
+
+    # bin_cam_instance.x *= -1
+    # bin_cam_mock.set_position(bin_cam_instance)
+
+    # for i in range(20*3):
+    #     sleep(0.05)
+    #     #print(f"{reader=}")
+    #     print(f"{reader.target_pose_yaw=}")
+
+
+
+
+if __name__ == '__main__':
+    main()
