65468f8fa378a56e72b3e76e5eed6e7f881ec65c
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2024-10-30 18:44:13 -0400

    Our iekf class

---
+++ mrobosub_localization/src/constants.py
@@ -0,0 +1,40 @@
+import numpy as np
+from scipy.linalg import block_diag
+from scipy.spatial.transform import Rotation
+
+imuHz = 20
+GRAVITY = np.array([0, 0, -9.81])
+
+# IMU
+std_acc_noise = 20 * 10**-6 * 9.8 * np.sqrt(imuHz)
+std_acc_bias_noise = 0.0001 * np.sqrt(imuHz)  # See https://arxiv.org/pdf/1402.5450.pdf
+std_gyro_noise = 0.005 * np.pi / 180 * np.sqrt(imuHz)
+std_gyro_bias_noise = 0.000618 * 8 / 18 * np.sqrt(imuHz)
+
+cov_acc_noise = np.eye(3) * (std_acc_noise**2)
+cov_acc_bias_noise = np.eye(3) * (std_acc_bias_noise**2)
+cov_gyro_noise = np.eye(3) * (std_gyro_noise**2)
+cov_gyro_bias_noise = np.eye(3) * (std_gyro_bias_noise**2)
+
+state_covariance = block_diag(
+    cov_gyro_noise,
+    cov_acc_noise,
+    np.zeros((3, 3)),
+    cov_gyro_bias_noise,
+    cov_acc_bias_noise,
+)
+
+# DVL
+std_dvl_noise = 0.0101 * 2.6
+cov_dvl_noise = np.eye(3) * (std_dvl_noise**2)
+# dvl_translation = np.array([0, 0, 0])
+# dvl_rotation = np.eye(3)
+dvl_translation = np.array([-0.17137, 0.00922, -0.33989])
+dvl_rotation = Rotation.from_euler('xyz', [6, 3, 90], degrees=True).as_matrix()
+
+# Depth Sensor
+std_depth_noise = 51 * 1 / 100 * 1 / 2
+cov_depth_noise = np.eye(3) * (std_depth_noise**2)
+depth_translation = np.array([0, 0, 0])
+
+measurement_covariance = np.diag([std_dvl_noise, std_dvl_noise, std_dvl_noise, std_depth_noise]) ** 2
