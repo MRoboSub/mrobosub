d85b06f5fdac05e3ef3061b967ecbf258c8c0cb8
Alex Bowler <albowler@umich.edu>
2024-11-12 18:10:25 +0000

    add dvl launch to hal launch and add timeout for socket if DVL data fails

--- mrobosub_hal/src/dvl_publisher.py
+++ mrobosub_hal/src/dvl_publisher.py
@@ -27,31 +27,37 @@ class DVLPublisher():
 
         # connect to socket containing the DVL information
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setblocking(False)
+        sock.settimeout(0.2)
         sock.bind((UDP_IP, UDP_PORT))
  
         while not rospy.is_shutdown():
-            data, addr = sock.recvfrom(1024)
-            data_str = data.decode()
+            try:
+                data, addr = sock.recvfrom(1024)
+                data_str = data.decode()
 
-            if not data_str.startswith("$DVKFC"):
+                if not data_str.startswith("$DVKFC"):
+                    rate.sleep()
+                    continue
+
+                # parse according to spec here https://docs.ceruleansonar.com/c/dvl-50/communicating-with-the-tracker-650/outgoing-message-formats-tracker-650-to-host/usddvkfc-kalman-filter-raw-data-support-message
+                data_list = data_str.split(',')
+                self.pub_raw.publish(Dvl(*map(lambda i: float(data_list[i]), (10, 17, 24))))
+
+                # transform from cone axis to tranlational axis 
+                cone_A_vel = float(data_list[10])
+                cone_B_vel = float(data_list[17])
+                cone_C_vel = float(data_list[24])
+                velocities = cone_A_vel * self.coneA + cone_B_vel * self.coneB + cone_C_vel * self.coneC
+                trans_response = Dvl()
+                trans_response.velocityA = float(velocities[0]) #along surge axis
+                trans_response.velocityB = float(velocities[1]) #along sway axis
+                trans_response.velocityC = float(velocities[2]) #along heave axis (positive down)
+                self.pub_translational.publish(trans_response)
                 rate.sleep()
-                continue
-
-            # parse according to spec here https://docs.ceruleansonar.com/c/dvl-50/communicating-with-the-tracker-650/outgoing-message-formats-tracker-650-to-host/usddvkfc-kalman-filter-raw-data-support-message
-            data_list = data_str.split(',')
-            self.pub_raw.publish(Dvl(*map(lambda i: float(data_list[i]), (10, 17, 24))))
-
-            # transform from cone axis to tranlational axis 
-            cone_A_vel = float(data_list[10])
-            cone_B_vel = float(data_list[17])
-            cone_C_vel = float(data_list[24])
-            velocities = cone_A_vel * self.coneA + cone_B_vel * self.coneB + cone_C_vel * self.coneC
-            trans_response = Dvl()
-            trans_response.velocityA = float(velocities[0]) #along surge axis
-            trans_response.velocityB = float(velocities[1]) #along sway axis
-            trans_response.velocityC = float(velocities[2]) #along heave axis (positive down)
-            self.pub_translational.publish(trans_response)
-            rate.sleep()
+            except socket.timeout:
+                print("DVL UDP connection timing out, no data recieved from DVL")
+        sock.close()
 
 if __name__ == "__main__":
     node = DVLPublisher()
