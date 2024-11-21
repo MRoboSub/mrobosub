eb9d7cb1f3f49445c3dd03ff7c0ce1a97e166117
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2024-11-20 22:03:53 -0500

    code review

--- mrobosub_monitoring/src/temperature_controller.py
+++ mrobosub_monitoring/src/temperature_controller.py
@@ -15,15 +15,19 @@ class TemperatureController:
         rospy.init_node("temperature_controller")
 
     def is_overheating(self) -> bool:
-        for _, temperatures in psutil.sensors_temperatures().items():
-            if any(temp.critical is not None and temp.current >= temp.critical for temp in temperatures):
-                return True
-        return False
+        return any(
+            temp.critical is not None and temp.current >= temp.critical 
+            for _, temperatures in psutil.sensors_temperatures().items() 
+            for temp in temperatures
+        )
 
     def shutdown(self):
-        for axis in ('surge', 'sway', 'heave', 'yaw', 'roll', 'pitch'):
-            rospy.Publisher(f"/output_wrench/{axis}", Float64, queue_size=1).publish(0)
-            rospy.Publisher(f"/target_twist/{axis}", Float64, queue_size=1).publish(0)
+        shutdown_rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            for axis in ('surge', 'sway', 'heave', 'yaw', 'roll', 'pitch'):
+                rospy.Publisher(f"/output_wrench/{axis}", Float64, queue_size=1).publish(0)
+                rospy.Publisher(f"/target_twist/{axis}", Float64, queue_size=1).publish(0)
+            shutdown_rate.sleep()
         rospy.signal_shutdown(f"Jetson Sensor Overheating Detected")
         time.sleep(2)
         os.system("sudo shutdown -h now")  # should run without needing password: %shutdown ALL=(root) NOPASSWD: /sbin/shutdown
@@ -34,7 +38,7 @@ class TemperatureController:
         last_check = None
         while not rospy.is_shutdown():
             if self.is_overheating():
-               last_check = last_check or rospy.get_time()
+                last_check = last_check or rospy.get_time()
                 if rospy.get_time() - last_check > OVERHEATING_TIME:
                     self.shutdown()
             else:
