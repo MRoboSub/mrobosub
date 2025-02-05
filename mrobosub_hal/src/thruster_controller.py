bbfd3dd9d9c6357b5050f1d7138dd096ebf086b8
Muskaan <muskaan@umich.edu>
2025-02-05 23:57:30 +0000

    work in progress

--- mrobosub_hal/src/thruster_controller.py
+++ mrobosub_hal/src/thruster_controller.py
@@ -8,25 +8,26 @@ from serial import Serial
 
 class ThrusterController(Node):
     def __init__(self):
+        # self.get_errors() #just to clear errors at the start
         rospy.Subscriber('/motor_pwm/0', Float64, self.motor_0_callback)
-        rospy.Subscriber('/motor_pwm/1', Float64, self.motor_0_callback)
-        rospy.Subscriber('/motor_pwm/2', Float64, self.motor_0_callback)
-        rospy.Subscriber('/motor_pwm/3', Float64, self.motor_0_callback)
-        rospy.Subscriber('/motor_pwm/4', Float64, self.motor_0_callback)
-        rospy.Subscriber('/motor_pwm/5', Float64, self.motor_0_callback)
-        rospy.Subscriber('/motor_pwm/6', Float64, self.motor_0_callback)
-        rospy.Subscriber('/motor_pwm/7', Float64, self.motor_0_callback)
+        rospy.Subscriber('/motor_pwm/1', Float64, self.motor_1_callback)
+        rospy.Subscriber('/motor_pwm/2', Float64, self.motor_2_callback)
+        rospy.Subscriber('/motor_pwm/3', Float64, self.motor_3_callback)
+        rospy.Subscriber('/motor_pwm/4', Float64, self.motor_4_callback)
+        rospy.Subscriber('/motor_pwm/5', Float64, self.motor_5_callback)
+        rospy.Subscriber('/motor_pwm/6', Float64, self.motor_6_callback)
+        rospy.Subscriber('/motor_pwm/7', Float64, self.motor_7_callback)
     
     # pwm_raw ranges from -1 to 1
     # pwm_val ranges from 4000 to 8000
-    def convert_pwm_signal(self, pwm_raw: Float64):
+    def convert_pwm_signal(self, pwm_raw: Float64) -> int:
         if(pwm_raw.data<-1 or pwm_raw.data>1):
             print(f"Thruster Controller Error: PWM value {pwm_raw} out of range (should be between -1 and 1 (both inclusive))")
             return -1
         return int((pwm_raw.data * 2000)+6000)
     
     # in case of invalid PWM or motor number parameters, does not send any updated signal to the motor controller and returns -1
-    def send_signal(self, motor: int, pwm_raw: Float64):
+    def send_signal(self, motor: int, pwm_raw: Float64) -> int:
         pwm_val: int = self.convert_pwm_signal(pwm_raw)
         if pwm_val == -1:
             return -1
@@ -35,11 +36,13 @@ class ThrusterController(Node):
             print(f"Thruster Controller Error: Motor number {motor} out of range (should be [0-7])")
             return -1
 
-        LSBs = pwm_val % (2**7) # TODO convert to uint8?
-        MSBs = int(pwm_val/(2**7)) #TODO convert to uint8?
+        LSBs = pwm_val % (2**7)
+        MSBs = int(pwm_val/(2**7))
 
         with Serial('/dev/ttyACM0') as s:
             s.write(bytearray([0xAA, 0x0C, 0x04, motor, LSBs, MSBs]))
+        
+        return 0
 
     def motor_0_callback(self, pwm_raw: Float64):
         self.send_signal(0, pwm_raw)
@@ -64,5 +67,12 @@ class ThrusterController(Node):
 
     def motor_7_callback(self, pwm_raw: Float64):
         self.send_signal(7, pwm_raw)
+
+    def get_errors(self):
+        # gets errors from thruster controller hardware (which automatically clears the errors too)
+        with Serial('/dev/ttyACM0') as s:
+            s.write(bytearray([0xAA, 0x0C, 0x21]))
+            error = s.read(2)
+            print(f"Thruster Controller: error code = {error}") #TODO how does error code get formatteed when you print it?
 
 
