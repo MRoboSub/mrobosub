000349ffe8aafd2c985a1c8d9d35a0b4bbca4956
bodee <bodee@umich.edu>
2025-03-12 19:45:06 -0400

    starting the state machine interaction with the path finder

---
+++ mrobosub_planning/src/state_machine/path_following.py
@@ -0,0 +1,6 @@
+# Go to nodes produced by path finder
+    # timeout: float = 150.0
+    # surge_speed: float = 0.15
+    # found_image_threshold = 50
+
+# This was found in gate_states line 45. use later
