067103e4a2a1d7427982708a7d8011bf80684fa7
Zayn Baig <zayn.baig@gmail.com>
2025-08-28 16:58:14 -0400

    Removes spurious comments and fixes prints

--- mrobosub_tests/mrobosub_tests/motor_test.py
+++ mrobosub_tests/mrobosub_tests/motor_test.py
@@ -8,30 +8,22 @@ from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy
 
 class MotorTest(Node):
     STOP_POWER = 0
-
     FORWARD_POWER = 0.1
     MOTOR_TIME_S = 5
     NUM_MOTORS = 8
 
     def __init__(self):
         super().__init__("motor_test")
-        #self.pub = rospy.Publisher("/motor_output", MotorState, queue_size=10, latch=True)
         qos_profile = QoSProfile(
             depth=10,
             history=HistoryPolicy.KEEP_LAST,
             reliability=ReliabilityPolicy.RELIABLE
         )
-
         self.pub = self.create_publisher(MotorState, "/motor_output", qos_profile)
-        #self.rate = rospy.Rate(1 / self.MOTOR_TIME_S)
         self.rate = self.create_rate(1 / self.MOTOR_TIME_S)
     
     def run(self):
         active_motor = 0
-
-        # not rospy.is_shutdown() --> rclpy.ok()
-        self.get_logger().info(f"{active_motor=}")
-        self.get_logger().info(f"{rclpy.ok()=}")
 
         while rclpy.ok() and active_motor < self.NUM_MOTORS:
             msg = MotorState()
@@ -41,7 +33,7 @@ class MotorTest(Node):
                 else:
                     msg.motors[i] = self.STOP_POWER
 
-            print(f"Running motor {active_motor} at {self.FORWARD_POWER}")
+            self.get_logger().info(f"Running motor {active_motor} at {self.FORWARD_POWER}")
             self.pub.publish(msg)
             self.rate.sleep()
 
@@ -52,4 +44,4 @@ class MotorTest(Node):
         for i in range(self.NUM_MOTORS):
             msg.motors[i] = self.STOP_POWER
         self.pub.publish(msg)
-        print("Motor test complete")
+        self.get_logger().info("Motor test complete")
