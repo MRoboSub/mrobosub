5bc2001e0784990f2722e09039fd5e893b5f1430
Zayn Baig <zayn.baig@gmail.com>
2025-09-21 03:06:06 -0400

    Clarifies the usage of mrobosub_lib.Node
    Most of the doc comments provided are not coherent with how we are using
    Node currently. I agree that we should keep mrobosub_lib.Node as a
    common ancestor of all nodes that we have. That said the comments were
    regarding using Node in a way that we (verbally) decided against after
    the comments were made. So I clarified them to be coherent with how we
    are using mrobosub_lib.Node right now. Later on, if this changes, doc
    comments can be added again.

--- mrobosub_lib/mrobosub_lib/__init__.py
+++ mrobosub_lib/mrobosub_lib/__init__.py
@@ -1,6 +1,5 @@
 """
-mrobosub_lib: Currently this package doesnt do much other than start the node and
-call run on a separate thread
+mrobosub_lib: Currently this package doesn't do much other than print the node is starting.
 """
 
 import rclpy
@@ -14,10 +13,4 @@ class Node(RosNode):
         # TODO: what about ros params?? we were parsing them here in ros1 but are now parsing them nowhere
 
     def run(self):
-        """
-        This function is intended to be called on a separate thread for an event loop
-
-        If `mrobosub_lib.main` is called, this function will be automatically executed while the node is being spun
-        Otherwise, self._start_thread() must be called to run this function in a separate thread
-        """
         pass
