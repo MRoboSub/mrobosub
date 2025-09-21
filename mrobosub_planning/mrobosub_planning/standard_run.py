7dfb752faf329e953ff51f466fbbe0e2a475f46f
Noah Mollerstuen <noah@mollerstuen.com>
2025-09-21 18:04:14 +0000

    Fix syntax errors

--- mrobosub_planning/mrobosub_planning/standard_run.py
+++ mrobosub_planning/mrobosub_planning/standard_run.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
-from mrobosub_planning.common_states import Start, Submerge, Surface, Stop, Surge
+from mrobosub_planning.common_states import Outcome, Start, Submerge, Surface, Stop, Surge, AlignToYaw
 from mrobosub_planning.umrsm import TransitionMap
 
 def make_move_forward():
-    class SurgeExtender(Surge): pass 
-    class AlignExtender(AlignToYaw): pass
+    class SurgeExtender(Surge):
+        class Surged(Outcome):
+            pass
+        class TimedOut(Outcome):
+            pass
+    
+    class AlignExtender(AlignToYaw):
+        class Aligned(Outcome):
+            pass
+        class TimedOut(Outcome):
+            pass
+    
     return SurgeExtender, AlignExtender
 
 Surge1, Align1 = make_move_forward()
@@ -21,24 +31,24 @@ transitions: TransitionMap = {
     Submerge.Submerged: Surge1.with_params(distance=20.),  # object distance
     Submerge.TimedOut: Surface,
 
-    Surge1.Surged: Align1.with_params(yaw=45.),
-    Align1.Aligned: Surge2.with_params(distance=10.)
+    Surge1.Surged: Align1.with_params(target_yaw=45.),
+    Align1.Aligned: Surge2.with_params(distance=10.),
     Align1.TimedOut: Surface,
 
-    Surge2.Surged: Align2.with_params(yaw=315.),
-    Align2.Aligned: Surge3.with_params(distance=10.)
+    Surge2.Surged: Align2.with_params(target_yaw=315.),
+    Align2.Aligned: Surge3.with_params(distance=10.),
     Align2.TimedOut: Surface,
 
-    Surge3.Surged: Align3.with_params(yaw=225.),
-    Align3.Aligned: Surge4.with_params(distance=10.)
+    Surge3.Surged: Align3.with_params(target_yaw=225.),
+    Align3.Aligned: Surge4.with_params(distance=10.),
     Align3.TimedOut: Surface,
 
-    Surge4.Surged: Align4.with_params(yaw=135.),
-    Align4.Aligned: Surge5.with_params(distance=10.)
+    Surge4.Surged: Align4.with_params(target_yaw=135.),
+    Align4.Aligned: Surge5.with_params(distance=10.),
     Align4.TimedOut: Surface,
 
-    Surge5.Surged: Align5.with_params(yaw=180.),
-    Align5.Aligned: Surge6.with_params(distance=20.)
+    Surge5.Surged: Align5.with_params(target_yaw=180.),
+    Align5.Aligned: Surge6.with_params(distance=20.),
     Align5.TimedOut: Surface,
 
     Surge6.Surged: Surface,
