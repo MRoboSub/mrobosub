d3e91d65c593b93a57c99aaab58f6544ea82c485
Alex Bowler <albowler@umich.edu>
2025-09-22 21:45:13 -0400

    First pass at migrating my files

--- mrobosub_perception/mrobosub_perception/save_calib_images.py
+++ mrobosub_perception/mrobosub_perception/save_calib_images.py
@@ -1,17 +1,16 @@
-#!/usr/bin/env python
-
-import rospy
+import rclpy
+from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 import os
 import threading
 
-class ImageSaverNode:
+class ImageSaverNode(Node):
     def __init__(self):
-        rospy.init_node('image_saver_node', anonymous=True)
+        super.__init__('image_saver_node', anonymous=True)
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber('/bot_cam', Image, self.image_callback)
+        self.image_sub = self.create_subscriber(Image, '/bot_cam', self.image_callback, qos_profile=1)
         self.image_count = 0
         self.save_directory = 'saved_images'
         self.save_flag = False
@@ -21,34 +20,31 @@ class ImageSaverNode:
             os.makedirs(self.save_directory)
 
         # Start save timer
-        self.save_timer = threading.Timer(3, self.set_save_flag)
-        self.save_timer.start()
+        self.save_timer = self.create_timer(3, self.set_save_flag)
 
     def image_callback(self, data):
-        rospy.loginfo("Received new image")
+        self.get_logger().info("Received new image")
         try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
             if self.save_flag:
+                cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
                 self.save_image(cv_image)
                 self.save_flag = False
         except Exception as e:
-            rospy.logerr("Error processing image: %s", str(e))
+            self.get_logger().error(f"Error processing image: {str(e)}")
 
     def set_save_flag(self):
         self.save_flag = True
-        self.save_timer = threading.Timer(3, self.set_save_flag)
-        self.save_timer.start()
 
     def save_image(self, image):
         filename = os.path.join(self.save_directory, f"image_{self.image_count}.jpg")
         cv2.imwrite(filename, image)
-        rospy.loginfo("Image saved: %s", filename)
+        self.get_logger().info(f"Image saved: {filename}")
         self.image_count += 1
 
-if __name__ == '__main__':
-    try:
-        image_saver_node = ImageSaverNode()
-        rospy.spin()
-    except rospy.ROSInterruptException:
-        pass
+def main():
+    rclpy.init()
+    node = ImageSaverNode()
+    rclpy.spin(node)
 
+if __name__ == '__main__':
+    main()
