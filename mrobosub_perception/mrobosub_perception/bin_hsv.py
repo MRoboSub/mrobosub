6332d57e799d30522337a2d7cc31de674d7ef856
Muskaan <muskaan@umich.edu>
2025-09-24 18:01:20 -0400

    work in progressm igraitng bin_hsv. params have not fully been migrated here either

--- mrobosub_perception/mrobosub_perception/bin_hsv.py
+++ mrobosub_perception/mrobosub_perception/bin_hsv.py
@@ -7,33 +7,32 @@ import sys
 
 from cv_bridge import CvBridge
 import rclpy
+from rclpy import utilities
 from mrobosub_msgs.srv import ObjectPosition, ObjectPositionResponse
 from timed_service import TimedService
-from dynamic_reconfigure.server import Server
 from sensor_msgs.msg import Image
-from mrobosub_lib.lib import Node, Param
-from mrobosub_perception.cfg import hsv_paramsConfig
+from mrobosub_lib.lib import Node
 
 from hsv_pipeline import HsvPipeline
 import utils
 
 class BinHsv(Node):
-    hsv_params: Param[dict]
     timing_threshold: Param[float]
 
     def __init__(self):
-        super().__init__('buoy_hsv')
+        super().__init__('bin_hsv')
+
+        self.declare_params()
 
         self.br = CvBridge()
 
-        self.always_run = rospy.myargv(sys.argv)[1] != "0" #input 1 for always_run to not have to do service calls always_run:=1
+        self.always_run = utilities.remove_ros_args(args=sys.argv)[1] != "0" #input 1 for always_run to not have to do service calls always_run:=1
 
         self.sub = self.create_subcriber(Image, '/rectified_image', self.handle_frame, qos_profile=1)
-        self.serv = TimedService('/bin_object_position', ObjectPosition, self.timing_threshold)
+        self.serv = TimedService(ObjectPosition, '/bin_object_position', self.timing_threshold)
         self.mask_pub = self.create_publisher(f'/bin_mask', Image, queue_size=1)
-        self.enhanced_pub = rospy.Publisher(f'/bin_enhanced', Image, queue_size=1)
-        self.annotated_pub = rospy.Publisher(f'/bin_annotated', Image, queue_size=1)
-        self.srv = Server(hsv_paramsConfig, self.reconfigure_callback, 'hsv_params')
+        self.enhanced_pub = self.create_publisher(Image, "/bin_enhanced", qos_profile=1)
+        self.annotated_pub = self.create_publisher(Image, "/bin_annotated",  qos_profile=1)
 
     def handle_frame(self, msg):
         if(self.serv.should_run() or self.always_run):
@@ -61,13 +60,36 @@ class BinHsv(Node):
                 response.y_theta = y_theta
 
             self.serv.set_result(response)
-        
 
-    def reconfigure_callback(self, config, level):
-        self.hsv_params.update({k: v for k, v in config.items() if k in self.hsv_params})
-        return config
+    def declare_params(self):
+        self.declare_parameters(
+            namespace='',
+            parameters=[
+                ('timing_threshold', rclpy.Parameter.Type.DOUBLE),
+                ('int_number', None),
+                ('float_number', None),
+                ('str_text', None),
+                ('bool_array', None),
+                ('int_array', None),
+                ('float_array', None),
+                ('str_array', None),
+                ('bytes_array', None),
+                ('nested_param.another_int', None)
+            ])
+        self.declare_parameter(
+            "timing_threshold",
+            ,
+        )
+        self.declare_parameter(
+            "hsv_params.hue_lo", rclpy.Parameter.Type.INTEGER_ARRAY)
+        
     
 
+def main():
+    rclpy.init(args=1)
+    node = BinHsv()
+    
 if __name__=='__main__' :
+
     node = BinHsv()
     rospy.spin()
