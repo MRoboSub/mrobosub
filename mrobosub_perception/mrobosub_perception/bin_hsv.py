5f58d757b86c9de0da4c3b5351e71e17308218c6
Alex Bowler <albowler@umich.edu>
2025-10-01 23:44:31 +0000

    Fix bin_hsv to run

--- mrobosub_perception/mrobosub_perception/bin_hsv.py
+++ mrobosub_perception/mrobosub_perception/bin_hsv.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 #hsv_filter
 from typing import Tuple
 import cv2
@@ -8,27 +6,29 @@ import sys
 from cv_bridge import CvBridge
 import rclpy
 from rclpy import utilities
-from mrobosub_msgs.srv import ObjectPosition, ObjectPositionResponse
-from timed_service import TimedService
+from rclpy.node import Node
+from mrobosub_msgs.srv import ObjectPosition
+from mrobosub_perception.timed_service import TimedService
 from sensor_msgs.msg import Image
-from mrobosub_lib.lib import Node
 
-from hsv_pipeline import HsvPipeline
-import utils
+from mrobosub_perception.hsv_pipeline import HsvPipeline
+import mrobosub_perception.utils as utils
 
 class BinHsv(Node):
-    timing_threshold: Param[float]
+    timing_threshold: float
 
     def __init__(self, always_run: bool):
         super().__init__('bin_hsv')
 
         self.declare_params()
+        self.timing_threshold = self.get_parameter('timing_threshold').get_parameter_value().double_value
+        self.get_logger().info(f"{self.timing_threshold=}")
 
         self.br = CvBridge()
 
         self.always_run = always_run
 
-        self.sub = self.create_subcriber(Image, '/rectified_image', self.handle_frame, qos_profile=1)
+        self.sub = self.create_subscription(Image, '/rectified_image', self.handle_frame, qos_profile=1)
         self.serv = TimedService(self, '/bin_object_position', ObjectPosition, self.timing_threshold)
         self.mask_pub = self.create_publisher(Image, f'/bin_mask', qos_profile=1)
         self.enhanced_pub = self.create_publisher(Image, f'/bin_enhanced', qos_profile=1)
@@ -62,25 +62,9 @@ class BinHsv(Node):
             self.serv.set_result(response)
 
     def declare_params(self):
-        self.declare_parameters(
-            namespace='',
-            parameters=[
-                ('timing_threshold', rclpy.Parameter.Type.DOUBLE),
-                ('int_number', None),
-                ('float_number', None),
-                ('str_text', None),
-                ('bool_array', None),
-                ('int_array', None),
-                ('float_array', None),
-                ('str_array', None),
-                ('bytes_array', None),
-                ('nested_param.another_int', None)
-            ])
         self.declare_parameter(
-            "timing_threshold",
+            "timing_threshold", 0.0
         )
-        self.declare_parameter(
-            "hsv_params.hue_lo", rclpy.Parameter.Type.INTEGER_ARRAY)
         
     
 def main():
--- mrobosub_perception/mrobosub_perception/draw_bboxes.py
+++ mrobosub_perception/mrobosub_perception/draw_bboxes.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import cv2
 import enum
 import torch
