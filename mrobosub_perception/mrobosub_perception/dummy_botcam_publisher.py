f69ee61cb224c3850999f5a7e918287127913c3c
usernamelin <usernameshort123@gmail.com>
2025-10-07 20:24:13 -0400

    dummy botcom publisher param

--- mrobosub_perception/mrobosub_perception/dummy_botcam_publisher.py
+++ mrobosub_perception/mrobosub_perception/dummy_botcam_publisher.py
@@ -1,24 +1,23 @@
 import rclpy
 from mrobosub_lib import Node
-from dynamic_reconfigure.server import Server
 from sensor_msgs.msg import Image
-from mrobosub_perception.cfg import dummy_botcam_paramsConfig
 from cv_bridge import CvBridge
 import cv2
 
 class DummyBotCam(Node):
     def __init__(self):
-        super().__init__('dummy_botcam')
+        super().__init__('dummy_botcam_publisher')
         self.br = CvBridge()
-        self.pub = self.create.publisher( Image, '/dummy_botcam', qos_profile=1)
+        self.pub = self.create_publisher( Image, '/dummy_botcam', qos_profile=1)
+        
         self.declare_parameter("image_number", 1)
-        self.srv = Server(dummy_botcam_paramsConfig, self.reconfigure_callback, 'dummy_botcam_config')
-        self.image_number = self.get_parameter('image_number').get_parameter_value().int_value
+        self.image_number = self.get_parameter('image_number').get_parameter_value().integer_value
         self.timer = self.create_timer(1, self.loop)
         
 
     def loop(self):
             img_path = f"../dummy_botcam_images/{self.image_number}.png"
+
             try:
                 cv_img = cv2.imread(img_path)
                 self.w = 480
@@ -28,6 +27,7 @@ class DummyBotCam(Node):
                 self.pub.publish(img_msg)
             except Exception as e:
                 self.get_logger().error(f"Failed to publish dummy botcam image {img_path}: {e}")
+                
 
 def main():
     rclpy.init()
