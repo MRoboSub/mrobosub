8b3e0bd73e52ddda5f7ec925b7aafdedf0a9f3f9
vivaansinghvi07 <vsinghvi@umich.edu>
2025-11-09 20:16:10 +0000

    added code to perform inference, for now testing on the botcam

--- mrobosub_perception/mrobosub_perception/dummy_botcam_publisher.py
+++ mrobosub_perception/mrobosub_perception/dummy_botcam_publisher.py
@@ -1,4 +1,5 @@
 import rclpy
+from pathlib import Path
 from mrobosub_lib import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
@@ -16,17 +17,18 @@ class DummyBotCam(Node):
         
 
     def loop(self):
-            img_path = f"../dummy_botcam_images/{self.image_number}.png"
+            img_path = f"{Path(__file__).parent}/bbox.png"
 
             try:
                 cv_img = cv2.imread(img_path)
                 self.w = 480
                 self.h = 640
+                cv_img = cv2.resize(cv_img, (self.h, self.w), interpolation=cv2.INTER_CUBIC)
                 cv_img = cv2.rectangle(cv_img, (self.w // 2 - 200 - 7, self.h // 2 - 200 + 4), (self.w // 2 + 200 - 7, self.h // 2 + 200 + 4), (255, 255, 255, 3))
                 img_msg = self.br.cv2_to_imgmsg(cv_img, encoding='bgr8')
                 self.pub.publish(img_msg)
             except Exception as e:
-                self.get_logger().error(f"Failed to publish dummy botcam image {img_path}: {e}")
+                self.get_logger().error(f"Failed to publish dummy botcam image {img_path}: {e}, {cv_img.shape}")
                 
 
 def main():
