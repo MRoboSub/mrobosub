c94c887a56681358018aa55f2129a291a46f1d33
Muskaan Mittal <mokamittal@gmail.com>
2025-11-18 20:39:30 -0500

    pid works

--- mrobosub_gnc/mrobosub_gnc/pid_dof_controller.py
+++ mrobosub_gnc/mrobosub_gnc/pid_dof_controller.py
@@ -3,11 +3,10 @@ import sys
 import argparse
 
 from mrobosub_lib import Node
-from rcl_interfaces.msg import ParameterDescriptor
 from std_msgs.msg import Float64
 
 INTEGRAL_DEADBAND = 15 # integral term only changes when pose within [setpoint - INTEGRAL_DEADBAND, setpoint + INTEGRAL_DEADBAND]
-# when outside this range, keep integral term constant
+# when pose outside this range, keep integral term constant
 
 class PidDofControlNode(Node):
     """
@@ -27,13 +26,13 @@ class PidDofControlNode(Node):
         self.declare_params()
         self.add_post_set_parameters_callback(self.set_params)
 
-        self.output = 0 # current output of PID algo
+        self.output = 0.0 # current output of PID algo
 
-        self.previous_valid = False
-        self.previous_pose = 0 # for D term
+        self.prev_valid = False
+        self.prev_pose = 0 # for D term
         self.prev_time = self.get_clock().now()
 
-        self.accumulated_error = 0 # accumulated integral of error
+        self.accumulated_error = 0.0 # accumulated integral of error
     
         self.pose = 0.0
         self.target_pose = 0.0 # setpoint
@@ -54,25 +53,22 @@ class PidDofControlNode(Node):
         )
         self.create_subscription(
             Float64, f"/target_twist/{dof_name}", self.target_twist, qos_profile=1
-        ) # subscribe to twist
+        )
 
-    def set_params(self, _params = None):
-        self.feed_forward = self.get_parameter('feed_forward').get_parameter_value().double_value
-    
+    def set_params(self, _params = None):    
         self.kp = self.get_parameter('kp').get_parameter_value().double_value
         self.kd = self.get_parameter('kd').get_parameter_value().double_value
         self.ki = self.get_parameter('ki').get_parameter_value().double_value
         self.angular = self.get_parameter('angular').value
         self.integral_windup_limit = self.get_parameter('integral_windup_limit').get_parameter_value().double_value # absolute value of max integral accumulated
-        # integral term does not get updated if update will take it outside [ - self.integral_windup_limit, self.integral_windup_limit ]
+        # integral term does not go outside [ - self.integral_windup_limit, self.integral_windup_limit ]
 
     def declare_params(self):
-        self.declare_parameter("feed_forward", 10)
-        self.declare_parameter("kp", 1)
-        self.declare_parameter("kd", 1)
-        self.declare_parameter("ki", 1)
+        self.declare_parameter("kp", 1.0)
+        self.declare_parameter("kd", 1.0)
+        self.declare_parameter("ki", 1.0)
         self.declare_parameter("angular", False)
-        self.declare_parameter("integral_windup_limit", 1)
+        self.declare_parameter("integral_windup_limit", 1.0)
         self.set_params()
 
     def target_pose_callback(self, target_pose: Float64):
@@ -82,8 +78,8 @@ class PidDofControlNode(Node):
     # returns the value in range [-180, 180)
     def convert_to_180_180_range(self, val):
         val = val % 360
-        if(val > 360):
-            val = 360 - val
+        if(val > 180):
+            val = val - 360
         return val
 
     def elapsed_ms(self, start, end):
@@ -91,51 +87,46 @@ class PidDofControlNode(Node):
 
     def pose_callback(self, pose: Float64):
         self.pose: float = pose.data
+        if not self.pid_enabled:
+            return
+
         error: float = self.target_pose - self.pose
 
         if self.angular:
-            # if input error % 360 will be in range [0, 360)
-            # if input error % 360 is < 180 then output +ve value
-            # if input error % 360 is >=180 then output -ve value
+            # (input error % 360) will be in range [0, 360)
+            # if (input error % 360) is < 180 then output +ve value
+            # if (input error % 360) is >=180 then output -ve value
             error = self.convert_to_180_180_range(error)
-            # now output error is in [-180, 180)
+            # now error is in [-180, 180)
 
-        if not self.previous_valid:
-            self.previous_valid = True
-            self.previous_pose = self.pose
+        if not self.prev_valid:
+            self.prev_valid = True
             self.accumulated_error = 0
 
         else:
             cur_time = self.get_clock().now()
             delta_time = self.elapsed_ms(self.prev_time, cur_time)
-            pose_diff: float = (self.pose - self.previous_pose) # don't do modulo anything, this should be the absolute value with sign and everything
-            derivative_term: float = pose_diff / delta_time
+            pose_diff: float = (self.pose - self.prev_pose) # don't do modulo anything, this should be the absolute value with sign and everything
+            derivative_term: float = - pose_diff / delta_time
 
-            # update self.accumulated error
-            if(abs(error) <= INTEGRAL_DEADBAND):
-                new_accumulated_error: float = self.accumulated_error + error * delta_time
-                if(abs(new_accumulated_error) <= abs(self.integral_windup_limit)):
-                    self.accumulated_error = new_accumulated_error
+            # update self.accumulated_error
+            if abs(error) <= INTEGRAL_DEADBAND:
+                self.accumulated_error += error * delta_time
 
-            effort: float = error * self.kp + derivative_term * self.kd + self.accumulated_error * self.ki
-            self.pid_callback(effort)
+                if(abs(self.accumulated_error) > abs(self.integral_windup_limit) and self.accumulated_error != 0):
+                    self.accumulated_error = self.accumulated_error / abs(self.accumulated_error) * abs(self.integral_windup_limit)
 
-        self.prev_time = self.get_clock().now() # setting this up for the next iteration     
+            self.output = error * self.kp + derivative_term * self.kd + self.accumulated_error * self.ki
+            self.output_pub.publish(Float64(data=self.output))
+
+        # setting up values for the next iteration
+        self.prev_time = self.get_clock().now() 
+        self.prev_pose = self.pose  
 
     def target_twist(self, target_twist: Float64):
         self.pid_enabled = False
-        self.previous_valid = False
-        self.pub_output(target_twist.data)
-
-    def pid_callback(self, effort: float): # wth is this function for
-        self.output = (
-            effort
-            + self.get_parameter("feed_forward").get_parameter_value().double_value
-        )
-        self.pub_output(self.output)
-
-    def pub_output(self, output: float):
-        self.output_pub.publish(Float64(data=output))
+        self.prev_valid = False
+        self.output_pub.publish(target_twist)
 
     def destroy_node(self):
         self.output_pub.publish(Float64(data=0.0))
@@ -150,7 +141,6 @@ def main():
 
     node = PidDofControlNode(args.dof_name)
     rclpy.spin(node)
-
 
 if __name__ == "__main__":
     main()
