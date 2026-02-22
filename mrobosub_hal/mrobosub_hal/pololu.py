d1ccc65c35af417f9455a3f72a3010bf34388c9e
Colten G <colteng@umich.edu>
2026-02-22 15:53:08 -0500

    Potentially functioning pololu.py and thruster_controller.py

--- mrobosub_hal/mrobosub_hal/pololu.py
+++ mrobosub_hal/mrobosub_hal/pololu.py
@@ -10,13 +10,12 @@ from std_srvs.srv import SetBool
 from typing import Optional
 
 
-NUM_MOTORS = 8
+NUM_PINS = 12
 
 
-class ThrusterController(Node):
+class Pololu(Node):
     """
-    Provides /emergency_stop_motors service
-    and requires /motor_output topic
+    Provides requires /pololu_commands topic
     """
 
     def __init__(self):
@@ -24,15 +23,16 @@ class ThrusterController(Node):
         self.get_logger().info("Launched thruster_controller node")
         self.port = "/dev/serial/by-id/usb-Pololu_Corporation_Pololu_Mini_Maestro_12-Channel_USB_Servo_Controller_00467345-if00"
         self.emergency_stop = False
-        self.motor_outputs = [0] * NUM_MOTORS
+        self.pololu_outputs = [0] * NUM_PINS
         self.serial: Serial | None = None
         self.connect()
         self.get_errors()  # clear errors at the start
 
-        self.object_position_service = self.create_service(
-            SetBool, "emergency_stop_motors", self.handle_emergency_stop
-        )
-        self.motor_sub = self.create_subscription(
+        #I don't think it's necessary to handle emergency stop here?
+        #self.object_position_service = self.create_service(
+        #    SetBool, "emergency_stop_motors", self.handle_emergency_stop
+        #)
+        self.pololu_sub = self.create_subscription(
             PololuCommands, "/pololu_commands", self.pololu_commands_callback, 1
         )
         self.timer = self.create_timer(1.0/50, self.loop)
@@ -72,15 +72,6 @@ class ThrusterController(Node):
             self.connect()
         return None
 
-    def handle_emergency_stop(
-        self, req: SetBool.Request, res: SetBool.Response
-    ) -> SetBool.Response:
-        self.emergency_stop = True
-        self.get_errors()
-        self.write(bytearray([0xAA, 0x0C, 0x22]))
-        res.success = True
-        return res
-
     # pwm_raw should be in [-1, 1]
     # pwm_val should be in [4000, 8000]
     def convert_pwm_signal(self, pwm_raw: float) -> Optional[int]:
@@ -91,39 +82,31 @@ class ThrusterController(Node):
             return None
         return int((pwm_raw * 1600) + 6000)
 
-    # in case of invalid PWM or motor number parameters, does not send any updated signal to the motor controller
-    def send_signal(self, motor: int, pwm_raw: float) -> int:
-        if self.get_parameter("thruster_reverse").get_parameter_value().bool_array_value[motor]:  # type: ignore
-            pwm_raw *= -1
+    # in case of invalid PWM or pin number parameters, does not send any updated signal to the pin controller
+    def send_signal(self, pin: int, pwm_raw: float) -> int:
         pwm_val = self.convert_pwm_signal(pwm_raw)
         if pwm_val is None:
             return -1
 
-        if motor < 0 or motor >= NUM_MOTORS:
+        if pin < 0 or pin >= NUM_PINS:
             self.get_logger().info(
-                f"ERROR: motor number {motor} out of range (should be in [0, {NUM_MOTORS-1}])"
+                f"ERROR: pin number {pin} out of range (should be in [0, {NUM_PINS-1}])"
             )
             return -1
-
-        motor = (
-            self.get_parameter("thruster_motor_id")
-            .get_parameter_value()
-            .integer_array_value[motor]
-        )
 
         LSBs = pwm_val % (2**7)
         MSBs = int(pwm_val / (2**7))
 
         self.get_errors()
-        self.write(bytearray([0xAA, 0x0C, 0x04, motor, LSBs, MSBs]))
-        # self.get_logger().info(f"Thruster controller: sent pwm value {pwm_val} to motor {motor}")
+        self.write(bytearray([0xAA, 0x0C, 0x04, pin, LSBs, MSBs]))
+        # self.get_logger().info(f"Thruster controller: sent pwm value {pwm_val} to pin {pin}")
 
         return 0
 
-    def motor_callback(self, msg: MotorState):
-        if not self.emergency_stop:
-            for i in range(NUM_MOTORS):
-                self.motor_outputs[i] = msg.motors[i]
+    def pololu_commands_callback(self, msg: PololuCommands):
+        for i in range(NUM_PINS):
+            if msg.valid[i]:
+                self.pololu_outputs[i] = msg.pins[i]
 
     def get_errors(self):
         # gets errors from thruster controller hardware (which automatically clears the errors too)
@@ -137,8 +120,8 @@ class ThrusterController(Node):
             # eg: error_code 16 means 00010000 which is the 5th error bit set
 
     def loop(self):
-        for i in range(NUM_MOTORS):
-            self.send_signal(i, self.motor_outputs[i])
+        for i in range(NUM_PINS):
+            self.send_signal(i, self.pololu_outputs[i])
 
 
 def main():
--- mrobosub_hal/mrobosub_hal/thruster_controller.py
+++ mrobosub_hal/mrobosub_hal/thruster_controller.py
@@ -21,18 +21,17 @@ class ThrusterController(Node):
     def __init__(self):
         super().__init__("thruster_controller")
         self.get_logger().info("Launched thruster_controller node")
-        self.port = "/dev/serial/by-id/usb-Pololu_Corporation_Pololu_Mini_Maestro_12-Channel_USB_Servo_Controller_00467345-if00"
         self.emergency_stop = False
         self.motor_outputs = [0] * NUM_MOTORS
-        self.serial: Serial | None = None
-        self.connect()
-        self.get_errors()  # clear errors at the start
 
         self.object_position_service = self.create_service(
             SetBool, "emergency_stop_motors", self.handle_emergency_stop
         )
         self.motor_sub = self.create_subscription(
             MotorState, "/motor_output", self.motor_callback, 1
+        )
+        self.pololu_pub = self.create_publisher(
+            PololuCommands, "/pololu_commands", qos_profile=1
         )
 
         self.declare_parameter(
@@ -42,6 +41,7 @@ class ThrusterController(Node):
                 description="List of booleans indicating whether each thruster is reversed"
             ),
         )
+
         self.declare_parameter(
             "thruster_motor_id",
             rclpy.Parameter.Type.INTEGER_ARRAY,
@@ -50,113 +50,71 @@ class ThrusterController(Node):
             ),
         )
 
-        self.timer = self.create_timer(1.0/50, self.loop)
+        #As far as I can tell there is no use for a loop 
+        #self.timer = self.create_timer(1.0/50, self.loop)
 
-    def connect(self) -> bool:
-        try:
-            self.serial = Serial(self.port, timeout=0.5, write_timeout=0.5)
-        except SerialException as e:
-            self.get_logger().info(f"Could not connect to mini maestro: {e}")
-            return False
-        return True
 
-    def write(self, data: bytearray) -> bool:
-        if self.serial is None:
-            success = self.connect()
-            if not success or self.serial is None:
-                return False
-        try:
-            self.serial.write(data)
-            return True
-        except SerialException as e:
-            self.get_logger().info(f"write error: {e}")
-            self.serial.close()
-            self.connect()
-        return False
-
-    def read(self, len: int) -> Optional[bytes]:
-        if self.serial is None:
-            success = self.connect()
-            if not success or self.serial is None:
-                return None
-        try:
-            return self.serial.read(len)
-        except SerialException as e:
-            self.get_logger().info(f"read error: {e}")
-            self.serial.close()
-            self.connect()
-        return None
-
+    # Not fully sure what this is doing
+    # Stripped out serial functionality and will just use emergency_stop
     def handle_emergency_stop(
         self, req: SetBool.Request, res: SetBool.Response
     ) -> SetBool.Response:
         if req.data:
             self.emergency_stop = True
-            self.get_errors()
-            self.write(bytearray([0xAA, 0x0C, 0x22]))
         else:
             self.emergency_stop = False
         res.success = True
         return res
 
-    # pwm_raw should be in [-1, 1]
-    # pwm_val should be in [4000, 8000]
-    def convert_pwm_signal(self, pwm_raw: float) -> Optional[int]:
-        if pwm_raw < -1 or pwm_raw > 1:
-            self.get_logger().info(
-                f"Thruster Controller [ERROR]: PWM value {pwm_raw} out of range (should be in [-1, 1])"
-            )
-            return None
-        return int((pwm_raw * 1600) + 6000)
 
     # in case of invalid PWM or motor number parameters, does not send any updated signal to the motor controller
-    def send_signal(self, motor: int, pwm_raw: float) -> int:
-        if self.get_parameter("thruster_reverse").get_parameter_value().bool_array_value[motor]:  # type: ignore
-            pwm_raw *= -1
-        pwm_val = self.convert_pwm_signal(pwm_raw)
-        if pwm_val is None:
-            return -1
+    def publish_motor_outputs(self, msg: MotorState) -> int:
+        message_valid = [False]*12
+        message_output = [0.0]*12
+        
+        if self.emergency_stop:
+            for motor in range(NUM_MOTORS):
 
-        if motor < 0 or motor >= NUM_MOTORS:
-            self.get_logger().info(
-                f"ERROR: motor number {motor} out of range (should be in [0, {NUM_MOTORS-1}])"
-            )
-            return -1
+                motor_pin = (self.get_parameter("thruster_motor_id")
+                .get_parameter_value()
+                .integer_array_value[motor])
 
-        motor = (
-            self.get_parameter("thruster_motor_id")
-            .get_parameter_value()
-            .integer_array_value[motor]
-        )
+                message_valid[motor_pin] = True
+                message_output[motor_pin] = 0
+        else:
+            for motor in range(NUM_MOTORS):
+                #This may not be needed but I don't know 
+                #If the message recieved is fully filled this can be deleted
+                if msg[motor] is None:
+                    continue
 
-        LSBs = pwm_val % (2**7)
-        MSBs = int(pwm_val / (2**7))
+                motor_pin = (self.get_parameter("thruster_motor_id")
+                .get_parameter_value()
+                .integer_array_value[motor])
 
-        self.get_errors()
-        self.write(bytearray([0xAA, 0x0C, 0x04, motor, LSBs, MSBs]))
+                message_valid[motor_pin] = True
+
+                if self.get_parameter("thruster_reverse").get_parameter_value().bool_array_value[motor]:  # type: ignore
+                    message_output[motor_pin] = -msg[motor]
+                else
+                    message_output[motor_pin] = msg[motor]
+
+
+        msg = PololuCommands
+        msg.pins = message_output
+        msg.valid = message_valid
+
+        self.pololu_pub.publish(msg)
         # self.get_logger().info(f"Thruster controller: sent pwm value {pwm_val} to motor {motor}")
 
         return 0
 
     def motor_callback(self, msg: MotorState):
-        if not self.emergency_stop:
-            for i in range(NUM_MOTORS):
-                self.motor_outputs[i] = msg.motors[i]
+        publish_motor_outputs(msg)
 
-    def get_errors(self):
-        # gets errors from thruster controller hardware (which automatically clears the errors too)
-        self.write(bytearray([0xAA, 0x0C, 0x21]))
-        error = self.read(2)
-        if error is None:
-            return
-        error_code = int.from_bytes(error, "little")
-        if error_code != 0:
-            self.get_logger().info(f"Thruster controller: error code = {error_code}")
-            # eg: error_code 16 means 00010000 which is the 5th error bit set
-
-    def loop(self):
-        for i in range(NUM_MOTORS):
-            self.send_signal(i, self.motor_outputs[i] if not self.emergency_stop else 0)
+    #As far as I can tell there is no use for a loop
+    #def loop(self):
+        #publish_motor_outputs()
 
 def main():
     rclpy.init()
