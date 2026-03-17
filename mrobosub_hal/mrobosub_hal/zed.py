257bc57b8c144fe4329689a219a2b2512f08a974
Zayn Baig <zayn.baig@gmail.com>
2026-03-17 23:41:05 +0000

    Fix zed_launch file to actually take in the argument.

--- mrobosub_hal/mrobosub_hal/zed.py
+++ mrobosub_hal/mrobosub_hal/zed.py
@@ -18,7 +18,7 @@ class Zed(Node):
     def __init__(self):
         super().__init__("zed")
         self.iteration_rate = 60
-        self.device_path = "/dev/zed2" # sys.argv[1]
+        self.device_path = sys.argv[1]
         self.on = False
         self.br = CvBridge()
         self.raw_pub = self.create_publisher(Image, "/zed/raw", qos_profile=1)
